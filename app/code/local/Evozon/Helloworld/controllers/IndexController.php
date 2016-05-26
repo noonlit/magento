3f95231659445a4570207232e1d3668a0b50a36d
Ilinca Dobre <ilinca.dobre@evozon.com>
2016-05-26 11:44:29 +0300

    [ilinca-develop] add layoutviewer helloworld example

--- app/code/local/Evozon/Helloworld/controllers/IndexController.php
+++ app/code/local/Evozon/Helloworld/controllers/IndexController.php
@@ -2,10 +2,14 @@
 
 class Evozon_Helloworld_IndexController extends Mage_Core_Controller_Front_Action {        
     public function indexAction() {
-        echo 'Hello World';
+        //echo 'Hello World';
+        $this->loadLayout();
+        $this->renderLayout();
     }
     
     public function goodbyeAction() {
-    echo 'Goodbye World!';
+    //echo 'Goodbye World!';
+        $this->loadLayout();
+        $this->renderLayout();
 }
 }
