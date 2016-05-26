7383b7a5dd1c9d44c4bbe10252f2204096aaa465
haidubogdan <haidubogdan@gmail.com>
2016-05-26 09:13:49 +0300

    .htaccess file fixed

---
+++ app/code/local/Evozon/Bogdan/Catalog/controllers/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CategoryController
+ *
+ * @author bogdanhaidu
+ */
+class Evozon_Bogdan_Catalog_CategoryController
+{
+
+    //put your code here
+}
