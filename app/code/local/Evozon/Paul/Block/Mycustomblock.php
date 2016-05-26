d8d069eb3ae55232abc7fcf3e4d654de9157a7e9
Paul Gotea <goteap@gmail.com>
2016-05-26 17:57:13 +0300

    first working attempt - banner is showing with xml

---
+++ app/code/local/Evozon/Paul/Block/Mycustomblock.php
@@ -0,0 +1,6 @@
+<?php
+
+class Evozon_Paul_Block_Mycustomblock extends Mage_Core_Block_Template
+{
+    
+}
---
+++ app/code/local/Evozon/Paul/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Evozon_Paul_IndexController extends Mage_Core_Controller_Front_Action
+{
+
+    public function indexAction()
+    {
+        echo "ASdasdasd";
+    }
+
+}
