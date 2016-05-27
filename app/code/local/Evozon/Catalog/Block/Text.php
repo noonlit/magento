e96070d639b3c94cf0eb548eafeb1954a47119c3
Andra B <julietfeynman@gmail.com>
2016-05-27 11:51:12 +0300

    [andra] something has been solved

---
+++ app/code/local/Evozon/Catalog/Block/Text.php
@@ -0,0 +1,11 @@
+<?php
+
+class Evozon_Catalog_Block_Text extends Mage_Catalog_Block_Category_View
+{
+
+    public function showText()
+    {
+        return 'x';
+    }
+
+}
--- app/code/local/Evozon/Catalog/Model/Category.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Evozon_Andra_Model_Category extends Mage_Core_Model_Abstract
-{
-    
-}
