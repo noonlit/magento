3317482ffcd5035a324a359dc07358f219ec2614
Andra B <julietfeynman@gmail.com>
2016-05-27 10:49:23 +0300

    [andra] well, I can totally see some text

---
+++ app/code/local/Evozon/Catalog/Block/Category/View.php
@@ -0,0 +1,11 @@
+<?php
+
+class Evozon_Catalog_Block_Category_View extends Mage_Catalog_Block_Category_View
+{
+    
+    public function getProductListHtml()
+    {
+        return "I'm totally seeing this";
+    }
+
+}
---
+++ app/code/local/Evozon/Catalog/Model/Category.php
@@ -0,0 +1,12 @@
+<?php
+
+class Evozon_Andra_Model_Category extends Mage_Core_Model_Abstract
+{
+
+    public function getProductCollection()
+    {
+        // instead of products, you get a very useful message 
+        return "This is the fanciest text ever.";
+    }
+    
+}
