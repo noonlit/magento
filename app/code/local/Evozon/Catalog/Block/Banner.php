7eda9406e502c478f3707e36dfb8710c84ee923f
Ilinca Dobre <ilinca.dobre@evozon.com>
2016-05-27 16:52:25 +0300

    [ilinca-develop]added log files

--- app/code/local/Evozon/Catalog/Block/Banner.php
+++ app/code/local/Evozon/Catalog/Block/Banner.php
@@ -5,6 +5,8 @@ class Evozon_Catalog_Block_Banner extends Mage_Core_Block_Template
 
     public function showText()
     {
+        Mage::log('nothing');
+        Mage::log('another nothing', null, 'addBlock_logs.log');
         return "nothing";
     }
 
