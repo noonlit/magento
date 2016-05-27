38977333a37c6a064117d0b2d6385171c7e691c8
Marius Adam <marius.adam@evozon.com>
2016-05-27 14:45:25 +0300

    [marius] alomost added block

---
+++ app/code/local/Evozon/Catalog/Block/Simpletext.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Description of SimpleText
+ *
+ * @author mariusadam
+ */
+class Evozon_Catalog_Block_Simpletext extends Mage_Core_Block_Abstract
+{
+
+    public function getSimpleText()
+    {die("aici");
+        return "This is the wanted sample text";
+    }
+
+}
