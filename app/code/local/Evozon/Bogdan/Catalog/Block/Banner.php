f3e41b7efac3f516092091a9e92eb719e29c3190
haidubogdan <haidubogdan@gmail.com>
2016-05-27 17:07:28 +0300

    Friday final push

--- app/code/local/Evozon/Bogdan/Catalog/Block/Banner.php
+++ app/code/local/Evozon/Bogdan/Catalog/Block/Banner.php
@@ -5,6 +5,15 @@ class Evozon_Bogdan_Catalog_Block_Banner extends Mage_Core_Block_Template
 
     public function showText()
     {
+//        $myVariable = 3;
+//        $myArray = arrary();
+//        $myObject = $myArray;
+//        $e = 0;
+        Mage::log('My log entry');
+        Mage::log('My log message: ' . $myVariable);
+        Mage::log($myArray);
+        Mage::log($myObject);
+        Mage::logException($e);
         return "PROMOTIE !!!";
     }
 
--- test.php
+++ test.php
@@ -14,7 +14,17 @@ $products = Mage::getModel("catalog/product")->getCollection(10)->addAttributeTo
 //var_dump($product);
 
 foreach ($products as $product) {
-    var_dump($product)."<br>";
-    die();
+    //var_dump($product) . "<br>";
+    //die();
     //echo $product->getData('created_at')."<br>";
 }
+
+//$myVariable = 3;
+//$myArray = arrary();
+//$myObject = $myArray;
+//$e = 0;
+//Mage::log('My log entry');
+//Mage::log('My log message: ' . $myVariable);
+//Mage::log($myArray);
+//Mage::log($myObject);
+//Mage::logException($e);
