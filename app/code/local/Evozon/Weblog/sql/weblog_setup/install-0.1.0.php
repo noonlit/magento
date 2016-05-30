e73529d21d86a911b7b411ffa7d5e858797f952d
Ilinca Dobre <ilinca.dobre@evozon.com>
2016-05-30 12:39:54 +0300

    [ilinca-develop] added weblog_setup scripts

---
+++ app/code/local/Evozon/Weblog/sql/weblog_setup/install-0.1.0.php
@@ -0,0 +1,25 @@
+<?php
+
+$installer = $this;
+$installer->startSetup();
+$table = $installer->getConnection()->newTable($installer->getTable('weblog/blogpost'))
+    ->addColumn('blogpost_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned' => true,
+        'nullable' => false,
+        'primary' => true,
+        'identity' => true,
+        ), 'Blogpost ID')
+    ->addColumn('title', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+        'nullable' => false,
+        ), 'Blogpost Title')
+    ->addColumn('post', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+        'nullable' => true,
+        ), 'Blogpost Body')
+    ->addColumn('date', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
+        ), 'Blogpost Date')
+    ->addColumn('timestamp', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
+        ), 'Timestamp')
+    ->setComment('Magentotutorial weblog/blogpost entity table');
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
