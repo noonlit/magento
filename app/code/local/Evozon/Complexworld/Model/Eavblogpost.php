fb71e6a026930d352cd91544aab2b2408b056c21
Ilinca Dobre <ilinca.dobre@evozon.com>
2016-05-31 12:40:26 +0300

    [ilinca-develop]added eav test model

---
+++ app/code/local/Evozon/Complexworld/Model/Eavblogpost.php
@@ -0,0 +1,11 @@
+<?php
+
+class Evozon_Complexworld_Model_Eavblogpost extends Mage_Core_Model_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('complexworld/eavblogpost');
+    }
+
+}
---
+++ app/code/local/Evozon/Complexworld/Model/Resource/Setup.php
@@ -0,0 +1,6 @@
+<?php
+
+class Evozon_Complexworld_Model_Resource_Setup extends Mage_Eav_Model_Entity_Setup
+{
+    
+}
