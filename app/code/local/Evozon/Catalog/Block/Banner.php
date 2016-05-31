cf419de5643f657baa83fb287ecbbceb46e52468
Ilinca Dobre <ilinca.dobre@evozon.com>
2016-05-31 15:17:58 +0300

    [ilinca-develop]changed evozon-showBanner to deal with Mage::registry and addFieldToSelect methods

--- app/code/local/Evozon/Catalog/Block/Banner.php
+++ app/code/local/Evozon/Catalog/Block/Banner.php
@@ -24,31 +24,27 @@ class Evozon_Catalog_Block_Banner extends Mage_Core_Block_Template
     public function showBanner()
     {
         // get the current category id
-        $layer = Mage::getSingleton('catalog/layer');
-        $category = $layer->getCurrentCategory();
-        $currentCategoryId = $category->getId();
-
+        $currentCategoryId = Mage::registry('current_category')->getId();
+       
         // get the banner ids from category_banner for the current category id
-        $categoryBannerCollection = Mage::getModel('evozon_catalog/categorybanner')
+        $bannerIds = Mage::getModel('evozon_catalog/categorybanner')
                 ->getCollection()
-                ->addFieldToFilter('category_id', $currentCategoryId);
-        if (!empty($categoryBannerCollection)) {
-            $bannerIds = [];
-            foreach ($categoryBannerCollection as $categoryBanner) {
-                $bannerIds [] = $categoryBanner->getBannerId();
-            }
-        }
-
-        // get the banner from banner table for the bannerIds
-        $banners = [];
-        $banner = Mage::getModel('evozon_catalog/banner');
-        foreach ($bannerIds as $bannerId) {
-            $banners[] = $banner->load($bannerId)->getText();
-        }
+                ->addFieldToSelect('banner_id')
+                ->addFieldToFilter('category_id', $currentCategoryId)
+                ->getdata();
+       
+        // get the banner from banner table for the bannerIds        
+        $banners = Mage::getModel('evozon_catalog/banner')
+                ->getCollection()
+                ->addFieldToSelect('text')
+                ->addFieldToFilter('id', array("in"=> $bannerIds))
+                ->getdata();
 
         // format to string
-        $bannerString = implode('<br>', $banners);
-
+        $bannerString = '';
+        foreach($banners as $banner) {
+            $bannerString .= implode('', $banner) . '<br>';
+        }
         return $bannerString;
     }
 
