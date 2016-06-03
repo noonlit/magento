997e39a3e0bb5c311459ed9311886a4d60854465
Andra B <julietfeynman@gmail.com>
2016-06-03 19:01:51 +0300

    [andra] configurable product working and showing up, need to add more attributes

--- app/code/local/Evozon/Catalog/data/evozon_catalog_setup/data-upgrade-0.1.2-0.1.3.php
+++ app/code/local/Evozon/Catalog/data/evozon_catalog_setup/data-upgrade-0.1.2-0.1.3.php
@@ -1,4 +1,5 @@
 <?php
+Mage::log('Started data-install-0.1.3', null, 'scripts.log');
 
 // (invisible) simple product
 
@@ -6,17 +7,17 @@ $date = new DateTime();
 Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
 
 $productRed = Mage::getModel('catalog/product');
-if (!$productRed->getIdBySku('sku91-ignivomous-carassius-auratus-red')) {
+if (!$productRed->getIdBySku('sku91-red-compels')) {
     try {
         $productRed
                 ->setStoreId(1)
                 ->setWebsiteIds(array(1))
-                ->setAttributeSetId(16)
+                ->setAttributeSetId(13)
                 ->setTypeId('simple')
-                ->setSku('sku91-ignivomous-carassius-auratus-red')
+                ->setSku('sku91-red-compels')
                 ->setCreatedAt($date->getTimestamp())
                 ->setUpdatedAt($date->getTimestamp())
-                ->setName('Fire-breathing fruit fly')
+                ->setName('The reddest of red tops')
                 ->setWeight(1)
                 ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
                 ->setTaxClassId(0) // tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
@@ -25,8 +26,8 @@ if (!$productRed->getIdBySku('sku91-ignivomous-carassius-auratus-red')) {
                 ->setCountryOfManufacture('RO')
                 ->setCost(100) // the cost is what the merchant pays
                 ->setPrice(200) // the price is what the customer pays
-                ->setDescription('This is the long description of the fire-breathing goldfish')
-                ->setShortDescription('This is the short description of the fire-breathing goldfish')
+                ->setDescription('This is the long description of the red top')
+                ->setShortDescription('This is the short description of the red top')
                 ->setStockData(array(
                     'use_config_manage_stock' => 0,
                     'manage_stock' => 1,
@@ -35,28 +36,28 @@ if (!$productRed->getIdBySku('sku91-ignivomous-carassius-auratus-red')) {
                     'is_in_stock' => 1,
                     'qty' => 999
                 ))
-                ->setCategoryIds(array(7, 25));
+                ->setCategoryIds(array(11));
         $productRed->save();
-        // Mage::log()
+        Mage::log('Added first product', null, 'scripts.log');
     } catch (Exception $e) {
-        Mage::log($e->getMessage());
+        Mage::log($e->getMessage(), null, 'scripts.log');
     }
 }
 
 // another (invisible) simple product
 
 $productGreen = Mage::getModel('catalog/product');
-if (!$productGreen->getIdBySku('sku91-ignivomous-carassius-auratus-green')) {
+if (!$productGreen->getIdBySku('sku91-hulk-green')) {
     try {
         $productGreen
                 ->setStoreId(1)
                 ->setWebsiteIds(array(1))
-                ->setAttributeSetId(16)
+                ->setAttributeSetId(13)
                 ->setTypeId('simple')
-                ->setSku('sku91-ignivomous-carassius-auratus-green')
+                ->setSku('sku91-hulk-green')
                 ->setCreatedAt($date->getTimestamp())
                 ->setUpdatedAt($date->getTimestamp())
-                ->setName('Fire-breathing fruit fly')
+                ->setName('The greenest of green tops')
                 ->setWeight(10)
                 ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
                 ->setTaxClassId(0) // tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
@@ -65,8 +66,8 @@ if (!$productGreen->getIdBySku('sku91-ignivomous-carassius-auratus-green')) {
                 ->setCountryOfManufacture('RO')
                 ->setCost(300) // the cost is what the merchant pays
                 ->setPrice(500) // the price is what the customer pays
-                ->setDescription('This is the long description of the fire-breathing goldfish')
-                ->setShortDescription('This is the short description of the fire-breathing goldfish')
+                ->setDescription('This is the long description of the green top')
+                ->setShortDescription('This is the short description of the green top')
                 ->setStockData(array(
                     'use_config_manage_stock' => 0,
                     'manage_stock' => 1,
@@ -75,32 +76,35 @@ if (!$productGreen->getIdBySku('sku91-ignivomous-carassius-auratus-green')) {
                     'is_in_stock' => 1,
                     'qty' => 999
                 ))
-                ->setCategoryIds(array(7, 25));
-        $product->save();
+                ->setCategoryIds(array(11));
+        $productGreen->save();
+        Mage::log('Added second product', null, 'scripts.log');
     } catch (Exception $e) {
-        Mage::log($e->getMessage());
+        Mage::log($e->getMessage(), null, 'scripts.log');
     }
 }
 
 // configurable product
 
 $configProduct = Mage::getModel('catalog/product');
-if (!$product->getIdBySku('sku91-ignivomous-carassius-auratus')) {
+if (!$product->getIdBySku('sku91-extremely-trendy-tops')) {
     try {
         $configProduct
                 ->setWebsiteIds(array(1))
-                ->setAttributeSetId(16)
+                ->setAttributeSetId(13) // clothing
                 ->setTypeId('configurable')
-                ->setSku('sku999-ignivomous-goldfish')
+                ->setSku('sku91-extremely-trendy-tops')
                 ->setCreatedAt($date->getTimestamp())
                 ->setUpdatedAt($date->getTimestamp())
-                ->setName('Configurable fire-breathing goldfish')
+                ->setName('Configurable trendy tops')
                 ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
                 ->setTaxClassId(0) // tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
                 ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
                 ->setCountryOfManufacture('RO')
-                ->setDescription('This is the long description of the fire-breathing goldfish')
-                ->setShortDescription('This is the short description of the fire-breathing goldfish')
+                ->setCost(100) // the cost is what the merchant pays
+                ->setPrice(200) // the price is what the customer pays
+                ->setDescription('This is the long description of the trendy tops')
+                ->setShortDescription('This is the short description of the trendy tops')
                 ->setStockData(array(
                     'use_config_manage_stock' => 0,
                     'manage_stock' => 1,
@@ -109,17 +113,18 @@ if (!$product->getIdBySku('sku91-ignivomous-carassius-auratus')) {
                     'is_in_stock' => 1,
                     'qty' => 999
                 ))
-                ->setCategoryIds(array(7, 25));
-
+                ->setCategoryIds(array(11));
 
         $configProduct->getTypeInstance()->setUsedProductAttributeIds(array(92)); // color  
         $configurableAttributesData = $configProduct->getTypeInstance()->getConfigurableAttributesAsArray();
         $configProduct->setCanSaveConfigurableAttributes(true);
         $configProduct->setConfigurableAttributesData($configurableAttributesData);
 
+        // product data
         $configurableProductsData = array();
         
-        $redProductId = Mage::getModel("catalog/product")->getIdBySku('sku91-ignivomous-carassius-auratus-red');
+        // red product data
+        $redProductId = Mage::getModel("catalog/product")->getIdBySku('sku91-red-compels');
         $configurableProductsData[$redProductId] = array(
             '0' => array(
                 'label' => 'Red', // attribute label
@@ -129,7 +134,10 @@ if (!$product->getIdBySku('sku91-ignivomous-carassius-auratus')) {
                 'pricing_value' => '200' // value for the pricing
             ),
         );
-        $configurableProductsData[''] = array(
+        
+        // green product data
+        $greenProductId = Mage::getModel("catalog/product")->getIdBySku('sku91-hulk-green');
+        $configurableProductsData[$greenProductId] = array(
             '0' => array(
                 'label' => 'Green', //attribute label
                 'attribute_id' => '92', // attribute ID of attribute 'color' in my store
@@ -141,7 +149,10 @@ if (!$product->getIdBySku('sku91-ignivomous-carassius-auratus')) {
         
         $configProduct->setConfigurableProductsData($configurableProductsData);
         $configProduct->save();
+        Mage::log('Added configurable product', null, 'scripts.log');
     } catch (Exception $e) {
-        Mage::log($e->getMessage());
+        Mage::log($e->getMessage(), null, 'scripts.log');
     }
 }
+
+Mage::log('Ended data-install-0.1.3', null, 'scripts.log');
