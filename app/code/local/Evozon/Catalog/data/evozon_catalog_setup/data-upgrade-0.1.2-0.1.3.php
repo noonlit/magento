2e270961702f381fb719f89a1ff12fa36713fb53
Andra B <julietfeynman@gmail.com>
2016-06-05 17:59:27 +0300

    [andra] configurable product installed and visible on its category page

--- app/code/local/Evozon/Catalog/data/evozon_catalog_setup/data-upgrade-0.1.2-0.1.3.php
+++ app/code/local/Evozon/Catalog/data/evozon_catalog_setup/data-upgrade-0.1.2-0.1.3.php
@@ -6,11 +6,8 @@ $date = new DateTime();
 
 Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
 
-
+// configurable product should have 4 attributes - color, size, gender, occasion - with option
 $model = Mage::getModel('catalog/product');
-
-// configurable product should have 4 attributes - color, size, gender, occasion - with options
-
 $options = array();
 
 // colors
@@ -54,42 +51,40 @@ foreach ($options['color'] as $colorKey => $color) {
                 $simpleProduct['short_description'] = "This is the short description for a {$colorKey} product for {$genderKey}s, appropriate for {$occasionKey} situations.";
                 $simpleProduct['description'] = "This is the long description for a {$colorKey} product for {$genderKey}s, appropriate for {$occasionKey} situations. "
                         . "It suspiciously resembles the short description, but, on close inspection, it becomes apparent that it is, in fact, longer.";
-                $simpleProducts[] = $simpleProduct;
+                $simpleProductsCfg[] = $simpleProduct;
             }
         }
     }
 }
 
-// set common options for products
-$model
-        ->setStoreId(1)
-        ->setWebsiteIds(array(1))
-        ->setAttributeSetId(13)
-        ->setTypeId('simple')
-        ->setCreatedAt($date->getTimestamp())
-        ->setUpdatedAt($date->getTimestamp())
-        ->setWeight(1)
-        ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
-        ->setTaxClassId(0) // tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
-        ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE)
-        ->setCost(100) // the cost is what the merchant pays
-        ->setPrice(200) // the price is what the customer pays
-        ->setStockData(array(
-            'use_config_manage_stock' => 0,
-            'manage_stock' => 1,
-            'min_sale_qty' => 1,
-            'max_sale_qty' => 2,
-            'is_in_stock' => 1,
-            'qty' => 999
-        ))
-        ->setCategoryIds(array(11));
-
-
-// set configurable options for products
+// build and save the simple products
 foreach ($simpleProductsCfg as $productCfg) {
-    if (!$model->getIdBySku($productCfg['sku'])) {
+    $productModel = Mage::getModel('catalog/product');
+
+    if (!$productModel->getIdBySku($productCfg['sku'])) {
         try {
-            $model
+            $productModel
+                    ->setStoreId(1)
+                    ->setWebsiteIds(array(1))
+                    ->setAttributeSetId(13)
+                    ->setTypeId('simple')
+                    ->setCreatedAt($date->getTimestamp())
+                    ->setUpdatedAt($date->getTimestamp())
+                    ->setWeight(1)
+                    ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
+                    ->setTaxClassId(0) // tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
+                    ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE)
+                    ->setCost(100) // the cost is what the merchant pays
+                    ->setPrice(200) // the price is what the customer pays
+                    ->setStockData(array(
+                        'use_config_manage_stock' => 0,
+                        'manage_stock' => 1,
+                        'min_sale_qty' => 1,
+                        'max_sale_qty' => 2,
+                        'is_in_stock' => 1,
+                        'qty' => 999
+                    ))
+                    ->setCategoryIds(array(11))
                     ->setSku($productCfg['sku'])
                     ->setColor($productCfg['color'])
                     ->setSize($productCfg['size'])
@@ -98,7 +93,7 @@ foreach ($simpleProductsCfg as $productCfg) {
                     ->setName($productCfg['name'])
                     ->setDescription($productCfg['description'])
                     ->setShortDescription($productCfg['short_description']);
-            $model->save();
+            $productModel->save();
             Mage::log("Added {$productCfg['sku']}", null, 'scripts.log');
         } catch (Exception $e) {
             Mage::log($e->getMessage(), null, 'scripts.log');
@@ -107,120 +102,74 @@ foreach ($simpleProductsCfg as $productCfg) {
 }
 
 
-/*
-  // women, red, XS, casual
-  $womenRedXSCasual = Mage::getModel('catalog/product');
-  if (!$womenRedXSCasual->getIdBySku('sku91-women-red-xs-casual')) {
-  try {
-  $womenRedXSCasual
-  ->setStoreId(1)
-  ->setWebsiteIds(array(1))
-  ->setAttributeSetId(13)
-  ->setTypeId('simple')
-  ->setSku('sku91-women-red-xs-casual')
-  ->setCreatedAt($date->getTimestamp())
-  ->setUpdatedAt($date->getTimestamp())
-  ->setName('The reddest of red tops')
-  ->setWeight(1)
-  ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
-  ->setTaxClassId(0) // tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
-  ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE)
-  ->setColor(28) // red
-  ->setGender(94) // women
-  ->setSize(81) // XS
-  ->setOccasion(31) // casual
-  ->setCountryOfManufacture('RO')
-  ->setCost(100) // the cost is what the merchant pays
-  ->setPrice(200) // the price is what the customer pays
-  ->setDescription('This is the long description of the red top for women')
-  ->setShortDescription('This is the short description of the red top for women')
-  ->setStockData(array(
-  'use_config_manage_stock' => 0,
-  'manage_stock' => 1,
-  'min_sale_qty' => 1,
-  'max_sale_qty' => 2,
-  'is_in_stock' => 1,
-  'qty' => 999
-  ))
-  ->setCategoryIds(array(11));
-  $womenRedXSCasual->save();
-  Mage::log('Added women, red, XS, casual', null, 'scripts.log');
-  } catch (Exception $e) {
-  Mage::log($e->getMessage(), null, 'scripts.log');
-  }
-  }
+// build and save the configurable product
+$cfgProductModel = Mage::getModel('catalog/product');
+if (!$cfgProductModel->getIdBySku('sku-trendy-top')) {
+    try {
+        $cfgProductModel
+                ->setWebsiteIds(array(1))
+                ->setAttributeSetId(13) // clothing
+                ->setTypeId('configurable')
+                ->setSku('sku-trendy-top')
+                ->setCreatedAt($date->getTimestamp())
+                ->setUpdatedAt($date->getTimestamp())
+                ->setName('Yet another configurable trendy top')
+                ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
+                ->setTaxClassId(0) // tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
+                ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
+                ->setCountryOfManufacture('RO')
+                ->setCost(100) // the cost is what the merchant pays
+                ->setPrice(200) // the price is what the customer pays
+                ->setDescription('This is the long description of the trendy top')
+                ->setShortDescription('This is the short description of the trendy top')
+                ->setStockData(array(
+                    'use_config_manage_stock' => 0,
+                    'manage_stock' => 1,
+                    'min_sale_qty' => 1,
+                    'max_sale_qty' => 2,
+                    'is_in_stock' => 1,
+                    'qty' => 999
+                ))
+                ->setCategoryIds(array(11));
 
+        // get ids of used attributes and set them as configurable
+        $colorId = $model->getResource()->getAttribute('color')->getAttributeId();
+        $sizeId = $model->getResource()->getAttribute('size')->getAttributeId();
+        $genderId = $model->getResource()->getAttribute('gender')->getAttributeId();
+        $occasionId = $model->getResource()->getAttribute('occasion')->getAttributeId();
+        $cfgProductModel->getTypeInstance()->setUsedProductAttributeIds(array($colorId, $sizeId, $genderId, $occasionId));
+        $cfgAttributesData = $cfgProductModel->getTypeInstance()->getConfigurableAttributesAsArray();
+        $cfgProductModel->setCanSaveConfigurableAttributes(true);
+        $cfgProductModel->setConfigurableAttributesData($cfgAttributesData);
 
- */
-// configurable product
+        // set product data
+        $cfgProductsData = array();
 
-/* $configProduct = Mage::getModel('catalog/product');
-  if (!$product->getIdBySku('sku91-extremely-trendy-top')) {
-  try {
-  $configProduct
-  ->setWebsiteIds(array(1))
-  ->setAttributeSetId(13) // clothing
-  ->setTypeId('configurable')
-  ->setSku('sku91-extremely-trendy-top')
-  ->setCreatedAt($date->getTimestamp())
-  ->setUpdatedAt($date->getTimestamp())
-  ->setName('Configurable trendy top')
-  ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
-  ->setTaxClassId(0) // tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
-  ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
-  ->setCountryOfManufacture('RO')
-  ->setCost(100) // the cost is what the merchant pays
-  ->setPrice(200) // the price is what the customer pays
-  ->setDescription('This is the long description of the trendy top')
-  ->setShortDescription('This is the short description of the trendy top')
-  ->setStockData(array(
-  'use_config_manage_stock' => 0,
-  'manage_stock' => 1,
-  'min_sale_qty' => 1,
-  'max_sale_qty' => 2,
-  'is_in_stock' => 1,
-  'qty' => 999
-  ))
-  ->setCategoryIds(array(11));
+        foreach ($simpleProductsCfg as $productCfg) {
+            $productId = $model->getIdBySku($productCfg['sku']);
 
-  $configProduct->getTypeInstance()->setUsedProductAttributeIds(array(92)); // color
-  $configurableAttributesData = $configProduct->getTypeInstance()->getConfigurableAttributesAsArray();
-  $configProduct->setCanSaveConfigurableAttributes(true);
-  $configProduct->setConfigurableAttributesData($configurableAttributesData);
+            foreach ($productCfg as $key => $value) {
+                if ($key == 'color' || $key == 'size' || $key == 'gender' || $key == 'occasion') {
+                    $attributeLabel = $model->getResource()->getAttribute($key)->getSource()->getOptionText($value);
+                    $attributeId = $model->getResource()->getAttribute($key)->getId();
 
-  // product data
-  $configurableProductsData = array();
+                    $cfgProductsData[$productId][] = array(
+                        'label' => $attributeLabel,
+                        'attribute_id' => $attributeId,
+                        'value_index' => $value,
+                        'is_percent' => '0',
+                        'pricing_value' => $model->getPrice()
+                    );
+                }
+            }
+        }
 
-  // red product data
-  $redProductId = Mage::getModel("catalog/product")->getIdBySku('sku91-red-compels');
-  $configurableProductsData[$redProductId] = array(
-  '0' => array(
-  'label' => 'Red', // attribute label
-  'attribute_id' => '92', // attribute ID of attribute 'color' in my store
-  'value_index' => '28', // value of 'Red' index of the attribute 'color'
-  'is_percent' => '0', // fixed/percent price for this option
-  'pricing_value' => '200' // value for the pricing
-  ),
-  );
-
-  // green product data
-  $greenProductId = Mage::getModel("catalog/product")->getIdBySku('sku91-hulk-green');
-  $configurableProductsData[$greenProductId] = array(
-  '0' => array(
-  'label' => 'Green', //attribute label
-  'attribute_id' => '92', // attribute ID of attribute 'color' in my store
-  'value_index' => '24', // value of 'Green' index of the attribute 'color'
-  'is_percent' => '0', // fixed/percent price for this option
-  'pricing_value' => '500' // value for the pricing
-  )
-  );
-
-  $configProduct->setConfigurableProductsData($configurableProductsData);
-  $configProduct->save();
-  Mage::log('Added configurable product', null, 'scripts.log');
-  } catch (Exception $e) {
-  Mage::log($e->getMessage(), null, 'scripts.log');
-  }
-  } */
+        $cfgProductModel->setConfigurableProductsData($cfgProductsData);
+        $cfgProductModel->save();
+        Mage::log('Added configurable product', null, 'scripts.log');
+    } catch (Exception $e) {
+        Mage::log($e->getMessage(), null, 'scripts.log');
+    }
+}
 
 Mage::log('Ended data-upgrade-0.1.3', null, 'scripts.log');
