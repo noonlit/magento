1119e2faf334744d40effb0ed1964098393c7a74
Ilinca Dobre <ilinca.dobre@evozon.com>
2016-06-08 13:36:01 +0300

    [ilinca-develop]added image to configurable product

--- app/code/local/Evozon/Catalog/data/evozon_catalog_setup/data-upgrade-0.2.0-0.2.1.php
+++ app/code/local/Evozon/Catalog/data/evozon_catalog_setup/data-upgrade-0.2.0-0.2.1.php
@@ -20,68 +20,68 @@ $configuration = array(
 $simpleProducts = [];
 foreach ($configuration['color'] as $color) {
     foreach ($configuration['size'] as $size) {
-            foreach ($configuration['length'] as $length) {
-                $simpleProduct = Mage::getModel('catalog/product');
-                $sku = 'ilinca-dress' . '-' . $color . '-' . $size . '-' . $length . '-' . '000';
-                $name = 'ilinca-dress' . '-' . $color . '-' . $size . '-' . $length;
-                $colorId = $simpleProduct->getResource()->getAttribute("color")->getSource()->getOptionId(ucfirst($color));
-                $sizeId = $simpleProduct->getResource()->getAttribute("size")->getSource()->getOptionId(strtoupper($size));
-                $lengthId = $simpleProduct->getResource()->getAttribute("length")->getSource()->getOptionId(ucwords($length));
-                $is_in_stock = 1;
-                if ($length === 'long' || $length === 'short' ||  $size === 'L') {
-                    $is_in_stock = 0;
-                }
+        foreach ($configuration['length'] as $length) {
+            $simpleProduct = Mage::getModel('catalog/product');
+            $sku = 'ilinca-dress' . '-' . $color . '-' . $size . '-' . $length . '-' . '000';
+            $name = 'ilinca-dress' . '-' . $color . '-' . $size . '-' . $length;
+            $colorId = $simpleProduct->getResource()->getAttribute("color")->getSource()->getOptionId(ucfirst($color));
+            $sizeId = $simpleProduct->getResource()->getAttribute("size")->getSource()->getOptionId(strtoupper($size));
+            $lengthId = $simpleProduct->getResource()->getAttribute("length")->getSource()->getOptionId(ucwords($length));
+            $is_in_stock = 1;
+            if ($length === 'long' || $length === 'short' || $size === 'L') {
+                $is_in_stock = 0;
+            }
 
-                if (!$simpleProduct->getIdBySku($sku)) {
-                    $simpleProduct
-                            ->setWebsiteIds(array(Mage::app()->getStore($storeId)->getWebsiteId()))
-                            ->setAttributeSetId(13)
-                            ->setTypeId('simple')
-                            ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE)
-                            ->setSku($sku)
-                            ->setName($name)
-                            ->setCreatedAt(strtotime('now'))
-                            ->setDescription('Ilinca Dress long description')
-                            ->setShortDescription('Ilinca Dress short description')
-                            ->setPrice('150')
-                            ->setTaxClassId(2)  
-                            ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
-                            ->setColor($colorId)
-                            ->setSize($sizeId)
-                            ->setLength($lengthId)
-                            ->setMetaTitle('ilinca-dress meta title')
-                            ->setMetaKeywords('')
-                            ->setMetaDescription('ilinca-dress meta description')
-                            ->setStockData(array(
-                                'use_config_manage_stock' => 0,
-                                'manage_stock' => 1,
-                                'min_sale_qty' => 1,
-                                'max_sale_qty' => 2,
-                                'is_in_stock' => $is_in_stock,
-                                'qty' => 5
-                            ))
-                            ->setCategoryIds(array(4, 13));
+            if (!$simpleProduct->getIdBySku($sku)) {
+                $simpleProduct
+                        ->setWebsiteIds(array(Mage::app()->getStore($storeId)->getWebsiteId()))
+                        ->setAttributeSetId(13)
+                        ->setTypeId('simple')
+                        ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE)
+                        ->setSku($sku)
+                        ->setName($name)
+                        ->setCreatedAt(strtotime('now'))
+                        ->setDescription('Ilinca Dress long description')
+                        ->setShortDescription('Ilinca Dress short description')
+                        ->setPrice('150')
+                        ->setTaxClassId(2)
+                        ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
+                        ->setColor($colorId)
+                        ->setSize($sizeId)
+                        ->setLength($lengthId)
+                        ->setMetaTitle('ilinca-dress meta title')
+                        ->setMetaKeywords('')
+                        ->setMetaDescription('ilinca-dress meta description')
+                        ->setStockData(array(
+                            'use_config_manage_stock' => 0,
+                            'manage_stock' => 1,
+                            'min_sale_qty' => 1,
+                            'max_sale_qty' => 2,
+                            'is_in_stock' => $is_in_stock,
+                            'qty' => 5
+                        ))
+                        ->setCategoryIds(array(4, 13));
 
-                    $image = 'ilinca-dress' . '-' . $color . '.png';
-                    $mediaArray = array(
-                        'thumbnail' => $image,
-                        'small_image' => $image,
-                        'image' => $image
-                    );
-                    $importDir = Mage::getBaseDir('media') . DS . 'evozon/catalog/product/ilinca-dress/';
+                $image = 'ilinca-dress' . '-' . $color . '.png';
+                $mediaArray = array(
+                    'thumbnail' => $image,
+                    'small_image' => $image,
+                    'image' => $image
+                );
+                $importDir = Mage::getBaseDir('media') . DS . 'evozon/catalog/product/ilinca-dress/';
 
-                    foreach ($mediaArray as $imageType => $fileName) {
-                        $filePath = $importDir . $fileName;
-                        if (file_exists($filePath)) {
-                            $simpleProduct->addImageToMediaGallery($filePath, $imageType, false);
-                        }
+                foreach ($mediaArray as $imageType => $fileName) {
+                    $filePath = $importDir . $fileName;
+                    if (file_exists($filePath)) {
+                        $simpleProduct->addImageToMediaGallery($filePath, $imageType, false);
                     }
-
-                    $simpleProduct->save();
-                    $simpleProducts[] = $simpleProduct;
                 }
+
+                $simpleProduct->save();
+                $simpleProducts[] = $simpleProduct;
             }
         }
+    }
 }
 
 //create configurable product
@@ -94,7 +94,7 @@ if (!$configurableProduct->getIdBySku('ilincadress13')) {
             ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
             ->setSku('ilincadress13')
             ->setName('ilinca dress')
-            ->setPrice('150')            
+            ->setPrice('150')
             ->setCreatedAt(strtotime('now'))
             ->setDescription('Ilinca Dress long description')
             ->setShortDescription('Ilinca Dress short description')
@@ -106,13 +106,28 @@ if (!$configurableProduct->getIdBySku('ilincadress13')) {
             ))
             ->setCategoryIds(array(4, 13));
 
+    $image = 'ilinca-dress-black.png';
+    $mediaArray = array(
+        'thumbnail' => $image,
+        'small_image' => $image,
+        'image' => $image
+    );
+    $importDir = Mage::getBaseDir('media') . DS . 'evozon/catalog/product/ilinca-dress/';
+
+    foreach ($mediaArray as $imageType => $fileName) {
+        $filePath = $importDir . $fileName;
+        if (file_exists($filePath)) {
+            $configurableProduct->addImageToMediaGallery($filePath, $imageType, false);
+        }
+    }
+
     // assigning associated product to configurable
     $colorAttributeId = Mage::getModel('eav/entity_attribute')->getIdByCode('catalog_product', 'color');
     $sizeAttributeId = Mage::getModel('eav/entity_attribute')->getIdByCode('catalog_product', 'size');
-    $lengthAttributeId = Mage::getModel('eav/entity_attribute')->getIdByCode('catalog_product', 'length');  
-    
+    $lengthAttributeId = Mage::getModel('eav/entity_attribute')->getIdByCode('catalog_product', 'length');
 
-    $configurableProduct->getTypeInstance()->setUsedProductAttributeIds(array($colorAttributeId, $sizeAttributeId, $lengthAttributeId));    
+
+    $configurableProduct->getTypeInstance()->setUsedProductAttributeIds(array($colorAttributeId, $sizeAttributeId, $lengthAttributeId));
     $configurableAttributesData = $configurableProduct->getTypeInstance()->getConfigurableAttributesAsArray();
 
     $configurableProduct->setCanSaveConfigurableAttributes(true);
@@ -145,7 +160,7 @@ if (!$configurableProduct->getIdBySku('ilincadress13')) {
         );
     }
 
-    $configurableProduct->setConfigurableProductsData($configurableProductsData);    
+    $configurableProduct->setConfigurableProductsData($configurableProductsData);
     $configurableProduct->save();
 }
 
