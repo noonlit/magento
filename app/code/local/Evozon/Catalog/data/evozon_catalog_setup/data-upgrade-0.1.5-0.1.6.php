fad3b3f39d4d005b0e8fc16f02120a5c426e5e02
Andra B <julietfeynman@gmail.com>
2016-06-08 10:39:41 +0300

    [andra] virtual product added

---
+++ app/code/local/Evozon/Catalog/data/evozon_catalog_setup/data-upgrade-0.1.5-0.1.6.php
@@ -0,0 +1,44 @@
+<?php
+
+Mage::log('Started data-upgrade-0.1.6', null, 'scripts.log');
+
+// add virtual product - warranty
+
+$date = new DateTime();
+
+Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+$productModel = Mage::getModel('catalog/product');
+if (!$productModel->getIdBySku('sku-virtually-guaranteed')) {
+    try {
+        $productModel
+                ->setWebsiteIds(array(1))
+                ->setAttributeSetId(14)
+                ->setTypeId('virtual')
+                ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
+                ->setSku('sku-virtually-guaranteed')
+                ->setName('Pre & Post-Mortem Warranty')
+                ->setCreatedAt($date->getTimeStamp())
+                ->setDescription('Warranty long description')
+                ->setShortDescription('Warranty short description')
+                ->setPrice(100)
+                ->setTaxClassId(0)
+                ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
+                ->setStockData(array(
+                    'use_config_manage_stock' => 0,
+                    'manage_stock' => 1,
+                    'min_sale_qty' => 1,
+                    'max_sale_qty' => 1,
+                    'is_in_stock' => 1,
+                    'qty' => 100
+                ))
+                ->setCategoryIds(array(24));
+        
+        $productModel->save();
+        Mage::log('Added virtual product', null, 'scripts.log');
+    } catch (Exception $e) {
+        Mage::log($e->getMessage(), null, 'scripts.log');
+    }
+}
+
+Mage::log('Ended data-upgrade-0.1.6', null, 'scripts.log');
