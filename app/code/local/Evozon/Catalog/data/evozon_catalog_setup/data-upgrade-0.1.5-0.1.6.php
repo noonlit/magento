82acc0dbce3f8e10f5f9646a4b24e45d2cc21f6e
aqu1no <onea_raul94@yahoo.com>
2016-06-08 16:42:18 +0300

    [Onea-Raul] all product

---
+++ app/code/local/Evozon/Catalog/data/evozon_catalog_setup/data-upgrade-0.1.5-0.1.6.php
@@ -0,0 +1,57 @@
+<?php
+// add downloadable product
+$date = new DateTime();
+Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+$productModel = Mage::getModel('catalog/product');
+if (!$productModel->getIdBySku('sku-book')) {
+    $productModel
+            ->setWebsiteIds(array(1))
+            ->setStoreId(1)
+            ->setAttributeSetId(10)
+            ->setTypeId('downloadable')
+            ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
+            ->setSku('sku-donwloadable')
+            ->setName('Ion Creanga')
+            ->setCreatedAt(strtotime('now'))
+            ->setDescription('Ion Creanga')
+            ->setShortDescription('Ion Creanga')
+            ->setPrice(30)
+            ->setTaxClassId(0)
+            ->setLinksTitle('Download')
+            ->setStatus(Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
+            ->setStockData(array(
+                'use_config_manage_stock' => 1,
+                'manage_stock' => 1,
+                'min_sale_qty' => 1,
+                'max_sale_qty' => 1,
+                'is_in_stock' => 1,
+                'qty' => 100
+            ))
+            ->setCategoryIds(array(22));
+    
+    $downloadableData = array(
+        'has_options' => '1',
+        'required_options' => '1',
+        'links_title' => 'Download links',
+        'links_purchased_separately' => '1',
+        'links_exist' => '1'
+    );
+    
+    foreach($downloadableData as $key => $value) {
+        $productModel->setData($key, $value);
+    } 
+    
+    $productModel->save();
+    
+    $linkModel = Mage::getModel('downloadable/link')
+            ->setProductId($productModel->getId())      
+            ->setNumberOfDownloads('0')
+            ->setIsShareable('0')
+            ->setTitle('Download links')
+            ->setDefaultTitle('Download links')
+            ->setLinkUrl('')
+            ->setLinkType('file')
+            ->setLinkFile('/evozon/iubita.jpg')
+            ->save();
+}
+Mage::log('Ended data-upgrade-0.1.7', null, 'scripts.log');
