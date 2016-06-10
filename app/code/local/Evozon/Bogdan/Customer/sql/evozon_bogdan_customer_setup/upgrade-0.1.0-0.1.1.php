4d783fd6d7b4982dcbc89ec15e0ef23dc64de555
haidubogdan <haidubogdan@gmail.com>
2016-06-10 10:32:38 +0300

    added cnp input to checkout register form

--- app/code/local/Evozon/Bogdan/Customer/sql/evozon_bogdan_customer_setup/upgrade-0.1.0-0.1.1.php
+++ app/code/local/Evozon/Bogdan/Customer/sql/evozon_bogdan_customer_setup/upgrade-0.1.0-0.1.1.php
@@ -1,51 +1,22 @@
 <?php
 
-//ADD ATTRIBUTE FOR FORM
 $installer = $this;
-$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
 $installer->startSetup();
-
-$cnpAttributes = array(
-    'group' => 'Customer',
-    'label' => 'CNP',
-    'backend' => '',
+$setup = Mage::getModel('customer/entity_setup', 'core_setup');
+$setup->addAttribute('customer', 'cnp', array(
     'type' => 'varchar',
-    'table' => '',
-    'frontend' => '',
+    'label' => 'CNP',
     'input' => 'text',
-    'frontend_class' => '',
-    'source' => '',
-    'required' => '1',
-    'user_defined' => '1',
+    'visible' => true,
+    'visible_on_front' => true,
+    'required' => false,
+    'user_defined' => true,   
+    'is_system' => 0,
     'default' => '',
-    'unique' => '1',
+    'unique' => true,
     'note' => '',
-    'visible_on_front' => '1',
-    'used_for_sort_by' => '8',
-    'position' => '8',
-//    'input_renderer' => NULL,
-//    'global' => '1',
-//    'visible' => '1',
-//    'searchable' => '1',
-//    'filterable' => '1',
-//    'comparable' => '1',
-//    'visible_on_front' => '0',
-//    'is_html_allowed_on_front' => '0',
-//    'is_used_for_price_rules' => '1',
-//    'filterable_in_search' => '1',
-//    'used_in_product_listing' => '0',
-//    'used_for_sort_by' => '0',
-//    'is_configurable' => '1',
-//    'apply_to' => 'simple',
-//    'visible_in_advanced_search' => '1',
-//    'position' => '1',
-//    'wysiwyg_enabled' => '0',
-//    'used_for_promo_rules' => '1',
-        )
-;
-
-$setup->addAttribute(1, 'cnp', $cnpAttributes);
-
+    'source' => ''  
+));
 $attribute = Mage::getSingleton('eav/config')->getAttribute('customer', 'cnp');
 $usedInForms = array(
     'adminhtml_checkout',
@@ -56,20 +27,54 @@ $usedInForms = array(
 );
 $attribute
         ->setData('used_in_forms', $usedInForms);
+$attribute->save();
+$installer->endSetup();
 
 
-$cnp = Mage::getModel('eav/config')->getAttribute('customer', 'cnp');
-$id = $cnp->getId();
+////ADD ATTRIBUTE FOR FORM
+//$installer = $this;
+//$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+//$installer->startSetup();
+//
+//$cnpAttributes = array(
+//    'group' => 'Customer',
+//    'label' => 'CNP',
+//    'backend' => '',
+//    'type' => 'varchar',
+//    'table' => '',
+//    'frontend' => '',
+//    'input' => 'text',
+//    'frontend_class' => '',
+//    'source' => '',
+//    'required' => '1',
+//    'user_defined' => '1',
+//    'default' => '',
+//    'unique' => '1',
+//    'note' => '',
+//    'visible_on_front' => '1',
+//    'used_for_sort_by' => '8',
+//    'position' => '8',
+////    'input_renderer' => NULL,
+////    'global' => '1',
+////    'visible' => '1',
+////    'searchable' => '1',
+////    'filterable' => '1',
+////    'comparable' => '1',
+////    'visible_on_front' => '0',
+////    'is_html_allowed_on_front' => '0',
+////    'is_used_for_price_rules' => '1',
+////    'filterable_in_search' => '1',
+////    'used_in_product_listing' => '0',
+////    'used_for_sort_by' => '0',
+////    'is_configurable' => '1',
+////    'apply_to' => 'simple',
+////    'visible_in_advanced_search' => '1',
+////    'position' => '1',
+////    'wysiwyg_enabled' => '0',
+////    'used_for_promo_rules' => '1',
+//        )
+//;
 
-//$attrData = array(
-//    'attribute_id' => $id,
-//    'website_id' => 1,
-//    'is_visible' => 1,
-//    'is_required' => 1,
-//    'default_value' => null,
-//    'multiline_count' => null
-//);
-//$installer->getConnection()->insert('customer_eav_attribute_website', $attrData);
 
 //$attrData2 = array(
 //    'fieldset_id'=>null,
@@ -78,6 +83,8 @@ $id = $cnp->getId();
 //    'sort_order' => 11,
 //);
 //$installer->getConnection()->insert('eav_form_element', $attrData2);
+//
+//
+//$installer->endSetup();
 
 
-$installer->endSetup();
