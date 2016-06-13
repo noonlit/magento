99176b0524bdabc7d1be18550744078aba0afb21
AndreiBodea0 <andreicalinb@gmail.com>
2016-06-13 17:37:29 +0300

    [feature/displayQA] answers table integrated

--- app/code/local/Evozon/Qa/Model/Answer.php
+++ app/code/local/Evozon/Qa/Model/Answer.php
@@ -1,8 +1,22 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Evozon_Qa_Model_Answer extends Mage_Core_Model_Abstract
+{
 
+    protected function _construct()
+    {
+        //sets the resource model class instance used for this model
+        $this->_init('evozon_qa/answer');
+    }
+
+    public function fetchAnser()
+    {
+        $collection = $this->getCollection();
+        $currentProductId = Mage::registry('current_product')->getId();
+        $collection->addFieldToFilter('product_id', $currentProductId)
+//                ->join(array('mediator' => 'mediator'), 'main_table.banner_id = mediator.banner_id')
+        ;
+        return $collection;
+    }
+
+}
