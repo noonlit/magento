d548b352b154b86ae1f6aa4d4b2985315a8428bd
AndreiBodea0 <andreicalinb@gmail.com>
2016-06-13 17:03:42 +0300

    [feature/displayQA]integrated answers table

--- app/code/local/Evozon/Qa/Model/Question.php
+++ app/code/local/Evozon/Qa/Model/Question.php
@@ -13,7 +13,9 @@ class Evozon_Qa_Model_Question extends Mage_Core_Model_Abstract
     {
         $collection = $this->getCollection();
         $currentProductId = Mage::registry('current_product')->getId();
-        $collection->addFieldToFilter('product_id', $currentProductId);
+        $collection->addFieldToFilter('product_id', $currentProductId)
+                ->join(array('mediator' => 'mediator'), 'main_table.banner_id = mediator.banner_id')
+        ;
         return $collection;
     }
 
