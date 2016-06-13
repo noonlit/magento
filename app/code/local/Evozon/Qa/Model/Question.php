30512341d58d67a801b8d2df7c9f6e01abfceb0d
AndreiBodea0 <andreicalinb@gmail.com>
2016-06-13 22:30:15 +0300

    [feature/displayQA] questions displayed with 1 query

--- app/code/local/Evozon/Qa/Model/Question.php
+++ app/code/local/Evozon/Qa/Model/Question.php
@@ -14,8 +14,11 @@ class Evozon_Qa_Model_Question extends Mage_Core_Model_Abstract
         $collection = $this->getCollection();
         $currentProductId = Mage::registry('current_product')->getId();
         $collection->addFieldToFilter('product_id', $currentProductId)
-//                ->join(array('mediator' => 'mediator'), 'main_table.banner_id = mediator.banner_id')
-        ;
+                ->join(array('answers' => 'evozon_qa/answer'), 'main_table.question_id = answers.question_id')
+                ->join(array('customer' => 'customer/entity'), 'main_table.customer_id = customer.entity_id')
+                ;
+        Mage::log($collection->getSelect()->__toString(), null, 'myLog.log');
+        Mage::log($collection->getData(), null, 'myLog.log');
         return $collection;
     }
 
