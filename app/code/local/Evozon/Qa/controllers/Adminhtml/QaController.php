ceb59a8b6068e16ad7ff49b249b975b1f0bd5939
Bogdan <haidu_bogdan@yahoo.com>
2016-06-13 21:38:43 +0300

    [admin_panel] working to put a custom block in admin

--- app/code/local/Evozon/Qa/controllers/Adminhtml/QaController.php
+++ app/code/local/Evozon/Qa/controllers/Adminhtml/QaController.php
@@ -12,14 +12,15 @@ class Evozon_Qa_Adminhtml_QaController extends Mage_Adminhtml_Controller_Action
         $block = $this->getLayout()
                 ->createBlock('core/text', 'example-block')
                 ->setText('<h1>This is a text block</h1>');
-//$htmlInput = $this->getLayout()->createBlock('evozon_bogdan_customer/form_newregister')->setTemplate('customer/form/cnp_input.phtml')->toHTml()
-//        $this->_addContent($block);
-//        $block2 = $this->getLayout()
-//                ->createBlock('evozon_qa_adminhtml/content')
-//                ->setText('<h1>This is a text block</h1>');
-//
-//        $this->_addContent($block2);
-        //Don't know what this does
+
+        $this->_addContent($block);
+        //var_dump($this->getLayout()->createBlock('evozon_qa_adminhtml/content'));
+        $block2 = $this->getLayout()
+                ->createBlock('evozon_qa_adminhtml/content')
+                ->setText('<h1>This is a text block</h1>');
+
+        $this->_addContent($block2);
+
         $this->_addBreadcrumb($this->__('Q A Management'), $this->__('Q A Management'));
         $this->renderLayout();
     }
